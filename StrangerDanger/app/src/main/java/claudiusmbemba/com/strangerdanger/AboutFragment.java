330de01e261076058216b54e28601f0d7c2d367e
Claudius Mbemba <mbembac@gmail.com>
2015-04-10 13:18:33 -0400

    working on site UI

---
+++ StrangerDanger/app/src/main/java/claudiusmbemba/com/strangerdanger/AboutFragment.java
@@ -0,0 +1,25 @@
+package claudiusmbemba.com.strangerdanger;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ClaudiusThaBeast on 4/10/15.
+ */
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        //empty constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+}
